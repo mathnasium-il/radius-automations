e59b49c6f23386f7531d2a0ae6d0c79520c66a2b
Jamal Riley <73957695+jamalmriley@users.noreply.github.com>
2023-11-14 14:21:54 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project-clover
+ Automated operations for Mathnasium centers in the greater Chicagoland area.
---
+++ local/constant_contact/logger.js
@@ -0,0 +1,2 @@
+// Logs communication to Radius
+// For actives, log to account page for active guardians and guardian page for secondary guardians
